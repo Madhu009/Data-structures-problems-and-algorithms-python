853afa243f8e7e36f25ea8c62f985144df402215
madhu009 <s.madhu.akp@gmail.com>
2017-09-23 17:32:10 +0530

    Circular linked list

---
+++ LinkedLists/CLL/main.py
@@ -0,0 +1,19 @@
+from LinkedList import LinkedList
+
+linkedlist = LinkedList()
+
+linkedlist.insertend(10)
+linkedlist.insertend(20)
+linkedlist.insertend(30)
+linkedlist.insertend(40)
+linkedlist.insertbefore(5)
+linkedlist.insertposition(50,6)
+linkedlist.deletehead()
+linkedlist.deleteposition(5)
+linkedlist.deletematched(20)
+
+position=linkedlist.getposition(10)
+print(position)
+
+linkedlist.printlist()
+
